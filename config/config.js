83f2a2dbf5c2751710836e0ed50cb8ff6f438f0c
tanmoyUNBchandra <tanmoy.chandra@tuteck.com>
2023-06-26 00:18:18 +0530

    config file added

---
+++ config/config.js
@@ -0,0 +1,13 @@
+// firebaseConfig.js
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDRrUVRi5m-fuBk45zt8LYQSiMjWP4-Cx8",
+  authDomain: "multi-stop-watches.firebaseapp.com",
+  projectId: "multi-stop-watches",
+  storageBucket: "multi-stop-watches.appspot.com",
+  messagingSenderId: "828912065120",
+  appId: "1:828912065120:web:65edc3e5ae64259c578105",
+  measurementId: "G-Y68N9Q6B7L",
+};
+
+export default firebaseConfig;
--- src/components/pages/stopwatch/stopwatch.js
+++ src/components/pages/stopwatch/stopwatch.js
@@ -22,17 +22,8 @@ import {
 import uuid from "react-native-uuid";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { themes } from "../../../themes/themes";
-import { query, where } from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDRrUVRi5m-fuBk45zt8LYQSiMjWP4-Cx8",
-  authDomain: "multi-stop-watches.firebaseapp.com",
-  projectId: "multi-stop-watches",
-  storageBucket: "multi-stop-watches.appspot.com",
-  messagingSenderId: "828912065120",
-  appId: "1:828912065120:web:65edc3e5ae64259c578105",
-  measurementId: "G-Y68N9Q6B7L",
-};
+import firebaseConfig from "../../../../config/config";
 
 initializeApp(firebaseConfig);
 
