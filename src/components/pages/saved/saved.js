74a007b31196326738818946b20ec3eccdd3ab38
tanmoyUNBchandra <tanmoy.chandra@tuteck.com>
2023-07-02 01:02:10 +0530

    saved page ui

--- src/components/pages/saved/saved.js
+++ src/components/pages/saved/saved.js
@@ -6,87 +6,165 @@ import {
   StyleSheet,
   ScrollView,
 } from "react-native";
+import * as Font from "expo-font";
 import { Card, Text, Button } from "react-native-paper";
 import { initializeApp } from "firebase/app";
-
+import { IconButton, MD3Colors } from "react-native-paper";
 import { themes } from "../../../themes/themes";
 const theme = themes.default; // Change this to select a different theme
 
 const Saved = () => {
-  useEffect(() => {}, []);
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  useEffect(() => {
+    const loadFonts = async () => {
+      await Font.loadAsync({
+        "PlusJakartaSans-Regular": require("../../../../assets/fonts/plus_jakarta_sans/PlusJakartaSans-Bold.ttf"),
+        "MuseoModerno-Bold": require("../../../../assets/fonts/MuseoModerno-Bold.ttf"),
+      });
+      setFontsLoaded(true);
+    };
+
+    loadFonts();
+  }, []);
 
   let data = [
     {
       elapsedSeconds: "0",
       id: "2b5e8761-1836-11ee-ab9e-23f8bcd16490",
-      name: "New Stopwatch",
+      name: "Evening Lunch",
       timestamp: "2023-07-01T17:39:02.999Z",
+      elapsedHour: "1h 10m",
+      elapsedSec: "10",
+    },
+    {
+      elapsedSeconds: "0",
+      id: "2b5e8761-1836-11ee-ab9e-23f8bcd16490",
+      name: "Gym Workout",
+      timestamp: "2023-07-01T17:39:02.999Z",
+      elapsedHour: "2h 5m",
+      elapsedSec: "10",
+    },
+    {
+      elapsedSeconds: "0",
+      id: "2b5e8761-1836-11ee-ab9e-23f8bcd16490",
+      name: "Night Study",
+      timestamp: "2023-07-01T17:39:02.999Z",
+      elapsedHour: "2h 15m",
+      elapsedSec: "10",
+    },
+    {
+      elapsedSeconds: "0",
+      id: "2b5e8761-1836-11ee-ab9e-23f8bcd16490",
+      name: "Last Meal",
+      timestamp: "2023-07-01T17:39:02.999Z",
+      elapsedHour: "0h 30m",
+      elapsedSec: "10",
+    },
+    {
+      elapsedSeconds: "0",
+      id: "2b5e8761-1836-11ee-ab9e-23f8bcd16490",
+      name: "Evening Lunch",
+      timestamp: "2023-07-01T17:39:02.999Z",
+      elapsedHour: "1h 7m",
+      elapsedSec: "10",
     },
     {
       elapsedSeconds: "0",
       id: "2b5e8761-1836-11ee-ab9e-23f8bcd16490",
       name: "New Stopwatch",
       timestamp: "2023-07-01T17:39:02.999Z",
+      elapsedHour: "5h 10m",
+      elapsedSec: "10",
     },
     {
       elapsedSeconds: "0",
       id: "2b5e8761-1836-11ee-ab9e-23f8bcd16490",
-      name: "New Stopwatch",
+      name: "Office to home",
       timestamp: "2023-07-01T17:39:02.999Z",
+      elapsedHour: "50h 10m",
+      elapsedSec: "10",
     },
     {
       elapsedSeconds: "0",
       id: "2b5e8761-1836-11ee-ab9e-23f8bcd16490",
-      name: "New Stopwatch",
+      name: "Swimming",
       timestamp: "2023-07-01T17:39:02.999Z",
+      elapsedHour: "50h 10m",
+      elapsedSec: "10",
     },
     {
       elapsedSeconds: "0",
       id: "2b5e8761-1836-11ee-ab9e-23f8bcd16490",
-      name: "New Stopwatch",
+      name: "Evening Lunch",
       timestamp: "2023-07-01T17:39:02.999Z",
+      elapsedHour: "50h 10m",
+      elapsedSec: "10",
     },
     {
       elapsedSeconds: "0",
       id: "2b5e8761-1836-11ee-ab9e-23f8bcd16490",
-      name: "New Stopwatch",
+      name: "Evening Lunch",
       timestamp: "2023-07-01T17:39:02.999Z",
+      elapsedHour: "50h 10m",
+      elapsedSec: "10",
     },
     {
       elapsedSeconds: "0",
       id: "2b5e8761-1836-11ee-ab9e-23f8bcd16490",
-      name: "New Stopwatch",
+      name: "Evening Lunch",
       timestamp: "2023-07-01T17:39:02.999Z",
-    },
-    {
-      elapsedSeconds: "0",
-      id: "2b5e8761-1836-11ee-ab9e-23f8bcd16490",
-      name: "New Stopwatch",
-      timestamp: "2023-07-01T17:39:02.999Z",
-    },
-    {
-      elapsedSeconds: "0",
-      id: "2b5e8761-1836-11ee-ab9e-23f8bcd16490",
-      name: "New Stopwatch",
-      timestamp: "2023-07-01T17:39:02.999Z",
+      elapsedHour: "50h 10m",
+      elapsedSec: "10",
     },
   ];
 
+  if (!fontsLoaded) {
+    return (
+      <>
+        <Text>Loading..</Text>
+      </>
+    );
+  }
   return (
     <View style={styles.container}>
       <ScrollView style={styles.scrollView}>
         <View style={styles.cardContainer}>
           {data.map((card) => {
+            const date = new Date(card.timestamp);
+
+            const options = {
+              year: "numeric",
+              month: "long",
+              day: "numeric",
+              hour: "numeric",
+              minute: "numeric",
+              hour12: true,
+            };
+
+            const formattedDateTime = date.toLocaleDateString("en-US", options);
+
             return (
               <Card style={styles.cardStyle} id={card.id}>
                 <Card.Content>
-                  <Text variant="titleLarge">{card.name}</Text>
-                  <Text variant="bodyMedium">Card content</Text>
+                  <Text variant="titleMedium" style={styles.stopWatchHeading}>
+                    {card.name}
+                  </Text>
+                  <Text variant="bodySmall" style={styles.stopWatchMetaData}>
+                    {formattedDateTime}
+                  </Text>
+                  <Text variant="headlineLarge" style={styles.stopWatchTime}>
+                    {card.elapsedHour}
+                  </Text>
                 </Card.Content>
-                {/* <Card.Actions>
-                  <Button>Cancel</Button>
-                  <Button>Ok</Button>
-                </Card.Actions> */}
+                <Card.Actions>
+                  <View style={styles.cardShare}>
+                    <Text style={styles.shareText}>Share</Text>
+                  </View>
+                  <View style={styles.cardDelete}>
+                    <Text style={styles.deleteText}>Delete</Text>
+                  </View>
+                </Card.Actions>
               </Card>
             );
           })}
@@ -114,6 +192,48 @@ const styles = StyleSheet.create({
     width: "48%", // Adjust the width as per your preference
     marginBottom: 16,
     backgroundColor: "white",
+  },
+
+  stopWatchHeading: {
+    fontFamily: "PlusJakartaSans-Regular",
+    color: theme.textColor_light,
+  },
+  stopWatchMetaData: {
+    // fontFamily: "PlusJakartaSans-Regular",
+    color: theme.textColor_light,
+    fontSize: 11,
+  },
+  stopWatchTime: {
+    marginTop: 10,
+    // fontSize
+    fontFamily: "MuseoModerno-Bold",
+    color: theme.textColor_dark,
+  },
+  cardShare: {
+    height: 30,
+    width: "47%",
+    backgroundColor: theme.buttonLight,
+    borderRadius: 9,
+    alignItems: "center",
+    paddingTop: 5,
+  },
+  cardDelete: {
+    height: 30,
+    width: "47%",
+    backgroundColor: theme.dangerLight,
+    borderRadius: 9,
+    alignItems: "center",
+    paddingTop: 5,
+  },
+  shareText: {
+    fontSize: 13,
+    fontFamily: "PlusJakartaSans-Regular",
+    color: theme.buttonColorPrimary,
+  },
+  deleteText: {
+    fontSize: 13,
+    fontFamily: "PlusJakartaSans-Regular",
+    color: theme.buttonColorDanger,
   },
 });
 
